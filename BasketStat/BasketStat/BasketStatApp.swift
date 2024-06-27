7194022d1291b48c103d5b1852f48ccc91f26175
gadisom <jeongwon.kim@gmail.com>
2024-06-27 23:33:34 +0900

    Add : 프로젝트 생성

---
+++ BasketStat/BasketStat/BasketStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasketStatApp.swift
+//  BasketStat
+//
+//  Created by 김정원 on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct BasketStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
