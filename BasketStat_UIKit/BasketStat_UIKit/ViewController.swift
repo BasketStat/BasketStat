5b7bff18975db293dd0dea69aa8451fcc74ac8a3
gadisom <jeongwon.kim@gmail.com>
2024-07-09 11:08:54 +0900

    fix: storyboard 버그 수정

--- BasketStat_UIKit/BasketStat_UIKit/ViewController.swift
+++ BasketStat_UIKit/BasketStat_UIKit/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
