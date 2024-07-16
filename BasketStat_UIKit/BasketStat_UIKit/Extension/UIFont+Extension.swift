6e9173eb057f45edc3df63ad660365a2ef857cda
gadisom <jeongwon.kim@gmail.com>
2024-07-16 22:21:40 +0900

    Add : font, aassets

---
+++ BasketStat_UIKit/BasketStat_UIKit/Extension/UIFont+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIFont+Extension.swift
+//  BasketStat_UIKit
+//
+//  Created by 김정원 on 7/16/24.
+//
+
+import Foundation
+import UIKit
+extension UIFont {
+    static var h0b = UIFont(name: "Pretendard-Bold", size: 24) ?? UIFont.systemFont(ofSize: 24, weight: .bold)
+}
